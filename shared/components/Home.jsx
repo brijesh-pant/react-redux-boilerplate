dd5aecfd1280c5108e1c80a4bceeefc4dc9daad1
BrijeshPant <pantbrijesh717@live.com>
2016-12-18 22:54:40 +0530

    create redux container component

--- shared/components/Home.jsx
+++ shared/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
+import { connect } from 'react-redux'
 
-const Home = () => {
-  return (
-    <div>Hello World</div>
-  )
+class HomeComponent extends React.Component {
+  static propTypes = {
+    todos: PropTypes.array,
+  }
+  render () {
+    const {
+      todos,
+    } = this.props
+
+    return (
+      <div>Hello World - {todos.get(0)}</div>
+    )
+  }
 }
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+})
+
+const Home = connect(mapStateToProps)(HomeComponent)
 
 export default Home
