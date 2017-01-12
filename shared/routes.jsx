8940e01b50a2802cf006bb88fc728e44318b6da4
BrijeshPant <pantbrijesh717@live.com>
2017-01-12 12:33:55 +0530

    corrections

--- shared/routes.jsx
+++ shared/routes.jsx
@@ -4,11 +4,10 @@ import {Route, Redirect} from 'react-router'
 import App from 'components/App'
 import Page from 'components/Page'
 import Home from 'components/Home'
-import LoginPage from 'components/LoginPage'
+
 export default (
   <Route component={App}>
     <Route path="/" component={Page}>
-      <Route path='login' component={LoginPage} />
       <Route path="home" component={Home} />
     </Route>
     <Redirect from="*" to="/" />
